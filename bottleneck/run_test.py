dea897c730373c85dee6ca366a39c6b223ef77f8
Ryan Grout <rgrout@continuum.io>
2015-07-27 17:11:26 -0500

    add bottleneck

---
+++ bottleneck/run_test.py
@@ -0,0 +1,10 @@
+import bottleneck
+import bottleneck.slow
+import bottleneck.tests
+import bottleneck.src
+
+# C extensions
+import bottleneck.reduce
+import bottleneck.nonreduce
+import bottleneck.nonreduce_axis
+import bottleneck.move
