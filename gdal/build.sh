388b5b36b7b78036cb8910e5a6d399ff11ce9a99
Ryan Grout <rgrout@continuum.io>
2015-07-27 16:35:13 -0500

    add gdal.

---
+++ gdal/build.sh
@@ -0,0 +1,4 @@
+$PYTHON setup.py build_ext --include-dirs $INCLUDE_PATH --library-dirs $LIBRARY_PATH --gdal-config gdal-config
+$PYTHON setup.py build_py
+$PYTHON setup.py build_scripts
+$PYTHON setup.py install
---
+++ gdal/run_test.py
@@ -0,0 +1,18 @@
+import sys
+
+import osgeo._gdal
+import osgeo._gdalconst
+import osgeo._ogr
+import osgeo._osr
+import osgeo
+import gdal
+import gdalconst
+import ogr
+import osr
+
+cnt = ogr.GetDriverCount()
+for i in range(cnt):
+    print(ogr.GetDriver(i).GetName())
+
+driver = gdal.GetDriverByName("netCDF")
+assert driver is not None
