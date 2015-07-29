e3229fc232e0efc845262789078ef5af78a483e2
Sam Gillingham <gillingham.sam@gmail.com>
2015-07-29 12:39:42 +1000

    added GEOS library for use by GDAL

---
+++ geos/bld.bat
@@ -0,0 +1,11 @@
+cmake -G "NMake Makefiles" -DCMAKE_PREFIX_PATH=%PREFIX% -DCMAKE_INSTALL_PREFIX:PATH=%PREFIX% -D CMAKE_BUILD_TYPE=Release .
+if errorlevel 1 exit 1
+
+nmake
+if errorlevel 1 exit 1
+
+nmake install
+if errorlevel 1 exit 1
+
+move %PREFIX%\bin\*.* %PREFIX%
+if errorlevel 1 exit 1
---
+++ geos/build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+if [ `uname` == Darwin ]; then
+	export CC=clang
+	export CXX=clang++
+fi
+
+./configure --prefix=$PREFIX
+
+make
+make install
