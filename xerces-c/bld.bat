c2c01135a46caffc3bf95afe30976eaa0458c722
Sam Gillingham <gillingham.sam@gmail.com>
2015-07-31 09:41:59 +1000

    handle building with VC9/VC10 depending on whether we are using Python2 or 3

--- xerces-c/bld.bat
+++ xerces-c/bld.bat
@@ -1,23 +1,29 @@
 
 if %ARCH% == 64 (
     set PLATF=x64
-	set OUTDIR=Win64
+    set OUTDIR=Win64
 ) else (
     set PLATF=Win32
-	set OUTDIR=Win32
+    set OUTDIR=Win32
 )
 
-msbuild projects\Win32\VC10\xerces-all\xerces-all.sln /t:all /p:Configuration=Release;Platform=%PLATF%
+if %PY3K% == 1 (
+    set VCVERSION=VC10
+) else (
+    set VCVERSION=VC9
+)
+
+msbuild projects\Win32\%VCVERSION%\xerces-all\xerces-all.sln /t:all /p:Configuration=Release;Platform=%PLATF%
 if errorlevel 1 exit 1
 
 REM implib and DLL
 mkdir %PREFIX%\lib
-copy Build\%OUTDIR%\VC10\Release\xerces-c_3.lib %PREFIX%\lib\
+copy Build\%OUTDIR%\%VCVERSION%\Release\xerces-c_3.lib %PREFIX%\lib\
 if errorlevel 1 exit 1
-copy Build\%OUTDIR%\VC10\Release\xerces-c_3_1.dll %PREFIX%\bin\
+copy Build\%OUTDIR%\%VCVERSION%\Release\xerces-c_3_1.dll %PREFIX%\bin\
 if errorlevel 1 exit 1
 
-REM Headers. Must be a better way...
+REM Headers. 
 echo .cpp > excludelist.txt
 mkdir %PREFIX%\include\xercesc
 xcopy /s /exclude:excludelist.txt src\xercesc %PREFIX%\include\xercesc
