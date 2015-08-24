bc5247bada0b44788c6708c0b31249dcccee76c1
Ryan Grout <rgrout@continuum.io>
2015-08-24 13:14:02 -0700

    add pycrypto

---
+++ pycrypto-2.6.1/run_test.py
@@ -0,0 +1,19 @@
+import Crypto.Cipher._AES
+import Crypto.Cipher._ARC2
+import Crypto.Cipher._ARC4
+import Crypto.Cipher._Blowfish
+import Crypto.Cipher._CAST
+import Crypto.Cipher._DES
+import Crypto.Cipher._DES3
+import Crypto.Cipher._XOR
+import Crypto.Hash._MD2
+import Crypto.Hash._MD4
+import Crypto.Hash._RIPEMD160
+import Crypto.Hash._SHA224
+import Crypto.Hash._SHA256
+import Crypto.Hash._SHA384
+import Crypto.Hash._SHA512
+import Crypto.Util._counter
+import Crypto.Util.strxor
+# Make sure this line from paramiko works
+from Crypto.PublicKey import DSA
