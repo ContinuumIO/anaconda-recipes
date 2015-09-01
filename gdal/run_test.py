50141d4a0b2a805015bed6f873ac5a6dbb04ec1e
Sam Gillingham <gillingham.sam@gmail.com>
2015-09-01 17:49:03 +1000

    added tests for HDF4, HDF5, GML, GEOS and Proj.4

--- gdal/run_test.py
+++ gdal/run_test.py
@@ -16,3 +16,29 @@ for i in range(cnt):
 
 driver = gdal.GetDriverByName("netCDF")
 assert driver is not None
+
+driver = gdal.GetDriverByName("HDF4")
+assert driver is not None
+
+driver = gdal.GetDriverByName("HDF5")
+assert driver is not None
+
+# only available when xerces-c++ successfully linked in
+driver = ogr.GetDriverByName("GML")
+assert driver is not None
+
+# now test for GEOS support
+ogr.UseExceptions()
+g1 = ogr.Geometry(wkt='POLYGON((0 0, 10 10, 10 0, 0 0))')
+g2 = ogr.Geometry(wkt='POLYGON((-90 -90, -90 90, 190 -90, -90 -90))')
+# following line raises exception if no GEOS
+g1.Contains(g1)
+
+# test for proj.4 support
+osr.UseExceptions()
+sr1 = osr.SpatialReference()
+sr1.ImportFromEPSG(4326) # lat long
+sr2 = osr.SpatialReference()
+sr2.ImportFromEPSG(28355) # GDA94 / MGA zone 55
+# following line raises exception if no proj.4
+trans = osr.CoordinateTransformation(sr1, sr2)
