eafb583ef71dc3688bf499eef8fc84546649d287
Ryan Grout <rgrout@continuum.io>
2015-09-09 14:34:18 -0500

    fix graphviz windows build script

--- graphviz/bld.bat
+++ graphviz/bld.bat
@@ -1,2 +1,10 @@
-xcopy %SRC_DIR$% %LIBRARY_PREFIX%
+xcopy /S %SRC_DIR% %LIBRARY_PREFIX%
+if errorlevel 1 exit 1
+
+REM move docs to correct place
+xcopy /S %LIBRARY_PREFIX%\share\graphviz\doc %LIBRARY_PREFIX%\share\doc\graphviz
+rd /S /Q %LIBRARY_PREFIX%\share\graphviz\doc
+if errorlevel 1 exit 1
+
+del %LIBRARY_PREFIX%\bld.bat
 if errorlevel 1 exit 1
