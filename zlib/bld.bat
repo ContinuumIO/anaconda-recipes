c765fc689369efbf2d5d3ba1d7e2c2a453b0377c
Ryan Grout <rgrout@continuum.io>
2015-09-09 14:48:55 -0500

    fix windows build script

--- zlib/bld.bat
+++ zlib/bld.bat
@@ -2,15 +2,15 @@ mkdir build
 cd build
 
 REM Configure step
-cmake -G "%CMAKE_GENERATOR%" -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% -DCMAKE_INSTALL_PREFIX:PATH=%LIBRARY_PREFIX% %SRC_DIR%
+cmake -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% -DCMAKE_INSTALL_PREFIX:PATH=%LIBRARY_PREFIX% %SRC_DIR%
 if errorlevel 1 exit 1
 
 REM Build step
-devenv %PKG_NAME%.sln /Build "%RELEASE_TARGET%"
+nmake
 if errorlevel 1 exit 1
 
 REM Install step
-devenv %PKG_NAME%.sln /Build "%RELEASE_TARGET%" /Project INSTALL
+nmake install
 if errorlevel 1 exit 1
 
 REM Some OSS libraries are happier if z.lib exists, even though it's not typical on Windows
