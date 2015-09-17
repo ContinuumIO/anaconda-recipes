5c212366703c62217d81db13a959e059187a3491
Ryan Grout <rgrout@continuum.io>
2015-09-17 14:37:15 -0500

    define CMAKE_GENERATOR

--- libnetcdf/bld.bat
+++ libnetcdf/bld.bat
@@ -2,7 +2,14 @@ mkdir %SRC_DIR%\build
 cd %SRC_DIR%\build
 
 set HDF5_DIR=%LIBRARY_PREFIX%\cmake
-cmake %SRC_DIR% -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX%  -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON -DENABLE_TESTS=OFF -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% -G"%CMAKE_GENERATOR%" 
+
+if "%ARCH%" == "64" (
+  set CMAKE_GENERATOR="Visual Studio 10 2010 Win64"
+) else (
+  set CMAKE_GENERATOR="Visual Studio 10 2010"
+)
+
+cmake %SRC_DIR% -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX%  -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON -DENABLE_TESTS=OFF -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% -G %CMAKE_GENERATOR%
 if errorlevel 1 exit 1
 
 cmake --build .
@@ -14,4 +21,3 @@ if errorlevel 1 exit 1
 REM remove msvc redist files
 cd %LIBRARY_BIN%
 rm msvc*.dll
-rm Microsoft.VC90.CRT.manifest
