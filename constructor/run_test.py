dd8c0abb39ef89aa4fabaf1b373927def6b4e532
Ilan Schnell <ilanschnell@gmail.com>
2016-04-07 23:34:25 -0500

    update a few recipes

--- constructor/run_test.py
+++ constructor/run_test.py
@@ -1,7 +1,7 @@
 import constructor
 
 print('constructor version:', constructor.__version__)
-assert constructor.__version__ == '1.1.0'
+assert constructor.__version__ == '1.2.0'
 
 import constructor.tests
 constructor.tests.main()
