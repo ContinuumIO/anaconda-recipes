6d5ad4360c752e76a8ea0c7486dfcda06954aec4
Ilan Schnell <ilanschnell@gmail.com>
2016-06-01 14:51:50 -0500

    update Qt and PyQt

--- qt/bld.bat
+++ qt/bld.bat
@@ -1,21 +1,43 @@
-cd qt-everywhere-opensource-src-%PKG_VERSION%
 set INCLUDE=%LIBRARY_INC%;%INCLUDE%
 set LIB=%LIBRARY_LIB%;%LIB%
-echo y | configure.exe -release -shared -opensource -prefix %LIBRARY_PREFIX% -fast -no-qt3support -openssl -webkit -system-zlib -system-libpng -system-libtiff -system-libjpeg
-bin\qmake -r QT_BUILD_PARTS="libs tools"
 
-robocopy .\ %LIBRARY_PREFIX%\ mkspecs
+cd qt-everywhere-opensource-src-%PKG_VERSION%
+
+echo y | configure.exe -prefix %LIBRARY_LIB%\qt4 ^
+                       -libdir %LIBRARY_LIB% ^
+                       -bindir %LIBRARY_BIN% ^
+                       -headerdir %LIBRARY_INC%\qt4 ^
+                       -datadir %LIBRARY_LIB%\qt4 ^
+                       -release ^
+                       -shared ^
+                       -opensource ^
+                       -fast ^
+                       -no-qt3support ^
+                       -nomake examples ^
+                       -nomake demos ^
+                       -nomake docs ^
+                       -openssl ^
+                       -webkit ^
+                       -system-zlib ^
+                       -system-libpng ^
+                       -system-libtiff ^
+                       -system-libjpeg
+
+bin\qmake -r QT_BUILD_PARTS="libs tools"
 
 jom -j%NUMBER_OF_PROCESSORS%
 if errorlevel 1 exit 1
 nmake install
 if errorlevel 1 exit 1
 
-rd /S /Q %LIBRARY_PREFIX%\phrasebooks
+rd /S /Q %LIBRARY_LIB%\qt4\phrasebooks
+rd /S /Q %LIBRARY_LIB%\qt4\bin
+rd /S /Q %LIBRARY_LIB%\qt4\tests
+del %LIBRARY_LIB%\qt4\q3porting.xml
 
 :: These are here to map cl.exe version numbers, which we use to figure out which
-::   compiler we are using, and which compiler consumers of Qt need to use, to MSVC
-::   year numbers, which is how qt identifies MSVC versions.
+:: compiler we are using, and which compiler consumers of Qt need to use, to MSVC
+:: year numbers, which is how qt identifies MSVC versions.
 :: Update this with any new MSVC compiler you might use.
 echo @echo 15=2008 >> msvc_versions.bat
 echo @echo 16=2010 >> msvc_versions.bat
@@ -28,7 +50,14 @@ FOR /F "tokens=1,2 delims==" %%i IN ('msvc_versions.bat') DO echo %CL_TEXT% | fi
 EXIT 1
 :FOUND
 
-mkdir %LIBRARY_PREFIX%\mkspecs\win32-msvc-default
-copy %LIBRARY_PREFIX%\mkspecs\win32-msvc%VSTRING:~0,4%\* %LIBRARY_PREFIX%\mkspecs\win32-msvc-default\
+mkdir %LIBRARY_LIB%\qt4\mkspecs\win32-msvc-default
+copy %LIBRARY_LIB%\qt4\mkspecs\win32-msvc%VSTRING:~0,4%\* %LIBRARY_LIB%\qt4\mkspecs\win32-msvc-default\
 
 copy "%RECIPE_DIR%\write_qtconf.bat" "%PREFIX%\Scripts\.qt-post-link.bat"
+
+:: Rename executables to avoid clashes with Qt5
+cd %LIBRARY_BIN%
+for /r "%LIBRARY_BIN%" %%f in (*.exe) do (
+    move %%~nf.exe %%~nf-qt4.exe
+    if errorlevel 1 exit 1
+)
--- qt/write_qtconf.bat
+++ qt/write_qtconf.bat
@@ -1,16 +1,19 @@
 @echo off
 setlocal enabledelayedexpansion
-REM This file should be run after installing the qt package into any
-REM    environment.  It makes it so Qt knows where to look for includes and
-REM    libs.  This script is copied in bld.bat to the post-link script for qt,
-REM    which means it is run after the Qt package gets linked, and sets up
-REM    the correct path for whatever Conda env you install Qt to.
+:: This file should be run after installing the qt package into any
+:: environment.  It makes it so Qt knows where to look for includes and
+:: libs.  This script is copied in bld.bat to the post-link script for qt,
+:: which means it is run after the Qt package gets linked, and sets up
+:: the correct path for whatever Conda env you install Qt to.
 pushd "%~dp0\..\"
 set "FORWARD_SLASHED_PREFIX=%CD:\=/%"
 if not exist "%CD%\Library" mkdir "%CD%\Library"
 if not exist "%CD%\Library\bin" mkdir "%CD%\Library\bin"
 echo [Paths] > "%CD%\Library\bin\qt.conf"
-echo Prefix = %FORWARD_SLASHED_PREFIX%/Library >> "%CD%\Library\bin\qt.conf"
-REM some things go looking in the prefix root (pyqt, for example)
+echo Prefix = %FORWARD_SLASHED_PREFIX%/Library/lib/qt4 >> "%CD%\Library\bin\qt.conf"
+echo Binaries = %FORWARD_SLASHED_PREFIX%/Library/bin >> "%CD%\Library\bin\qt.conf"
+echo Libraries = %FORWARD_SLASHED_PREFIX%/Library/lib >> "%CD%\Library\bin\qt.conf"
+echo Headers = %FORWARD_SLASHED_PREFIX%/Library/include/qt4 >> "%CD%\Library\bin\qt.conf"
+:: Some things go looking in the prefix root (pyqt, for example)
 COPY %CD%\Library\bin\qt.conf %CD%\qt.conf
 @echo on
