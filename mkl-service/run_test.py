755b11b1c9b16184a8b14f26370ae853800d8726
Ilan Schnell <ilanschnell@gmail.com>
2016-06-02 16:09:07 -0500

    add mkl-service and simplegeneric

---
+++ mkl-service/run_test.py
@@ -0,0 +1,10 @@
+import mkl
+import mkl.test
+
+for s in ('mkl.get_max_threads()', 'mkl.get_cpu_clocks()', 'mkl.mem_stat()',
+          'mkl.get_cpu_frequency()', 'mkl.get_version_string()'):
+    print('%s=%r' % (s, eval(s)))
+
+assert mkl.test.run().wasSuccessful()
+assert 'Math Kernel Library Version 11.3.3 ' in mkl.get_version_string()
+assert mkl.__version__ == '1.1.2'
---
+++ simplegeneric/run_test.py
@@ -0,0 +1,1 @@
+from simplegeneric import generic
