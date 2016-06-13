38f12ee4e46a87b99066d93fec102d7b376f21b9
Ilan Schnell <ilanschnell@gmail.com>
2016-06-13 13:03:43 -0500

    add readline, update xlsxwriter

---
+++ readline-6.2/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ `uname -m` == ppc64le ]; then
+    B="--build=ppc64le-linux"
+fi
+
+./configure $B --prefix=$PREFIX
+make
+make install
+
+rm -rf $PREFIX/share/man
+rm -rf $PREFIX/share/readline
