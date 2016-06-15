8169f5708f677385fb42fdbe65e3a1c1612fdf5b
Rahul Jain <rjain@continuum.io>
2016-06-15 12:56:06 -0500

    Modified build.py to be able to skip already OK builds in results.txt and add argument if you only want to build 1 pkg

--- BUILD/build.py
+++ BUILD/build.py
@@ -1,36 +1,46 @@
 import os
-import sys
+import sys, getopt
 import json
 import yaml
 import subprocess
 from os.path import isfile, join
 from optparse import OptionParser
-
+import argparse
 
 PY_ALLOWED = ('27', '35')
-
+OK = []
+ALLOWED = []
 
 def get_recipes(filter):
     with open('support.json') as fi:
         supported = json.load(fi)
-
+    result = 'result.txt'
+    if isfile(result):
+        with open(result) as fi:
+            for line in fi:
+                line = line.split()
+                if(line[1] == "OK"):
+                    OK.append(line[0])		    			  
     for dn in sorted(os.listdir('..')):
-        recipe_dir = join('..', dn)
-        meta_path = join(recipe_dir, 'meta.yaml')
-        if not isfile(meta_path):
-            continue
-        with open(meta_path) as fi:
-            data = fi.read()
-        name = yaml.load(data)['package']['name']
-        if name not in supported:
-            continue
-        sl = supported[name]
-        if sl == 'all':
-            yield recipe_dir, name
-        else:
-            assert isinstance(sl, list)
-            if filter in sl:
+        if(dn not in OK):
+            recipe_dir = join('..', dn)
+            meta_path = join(recipe_dir, 'meta.yaml')
+            if not isfile(meta_path):
+                continue
+            with open(meta_path) as fi:
+                data = fi.read()
+            name = yaml.load(data)['package']['name']
+            if name not in supported:
+                continue
+            sl = supported[name]
+            if sl == 'all':
+                ALLOWED.append(name)
                 yield recipe_dir, name
+            else:
+                assert isinstance(sl, list)
+                if filter in sl:
+                    ALLOWED.append(name)
+                    yield recipe_dir, name
 
 
 def build(recipe_dir, py_ver='27'):
@@ -63,8 +73,8 @@ def main():
                  help="Python version to build against, defaults to %default")
 
     opts, args = p.parse_args()
-    if args:
-        p.error("No arguments expected")
+    if len(args)> 1:
+        p.error("Only zero or one arguement expected")
     if opts.py not in PY_ALLOWED:
         p.error("Did not expect --py=%s not in: %s" % (opts.py,
                                                        PY_ALLOWED))
@@ -73,14 +83,29 @@ def main():
     print('filter: %s' % filter)
 
     for recipe_dir, name in get_recipes(filter):
-        try:
-            build(recipe_dir, opts.py)
-            result = 'OK'
-        except subprocess.CalledProcessError:
-            result = 'FAILED'
-        with open('result.txt', 'a') as f:
-            f.write('%-20s %s\n' % (name, result))
-
-
+        if(len(args) == 1):
+            if(1):
+                if(name == args[0]):
+                    try:
+                        build(recipe_dir, opts.py)
+                        result = 'OK'
+                    except subprocess.CalledProcessError:
+                        result = 'FAILED'
+                    with open('result.txt', 'a') as f:
+                        f.write('%-20s %s\n' % (name, result))
+        else:
+            try:
+                build(recipe_dir, opts.py)
+                result = 'OK'
+            except subprocess.CalledProcessError:
+                result = 'FAILED'
+            with open('result.txt', 'a') as f:                 
+                f.write('%-20s %s\n' % (name, result))
+    
+    if(len(args) == 1):
+        if(args[0] not in ALLOWED):
+            if(args[0] in OK):
+                p.error("%s already OK in results.txt - To run again, clear it from results.txt" % (args[0]))
+            p.error("%s not found in directory" % (args[0]))
 if __name__ == '__main__':
     main()
