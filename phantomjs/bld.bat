1ead2337a147f5ea1a72dac84d884eae1326be9f
Ilan Schnell <ilanschnell@gmail.com>
2016-07-24 18:19:58 -0500

    add recipes for phantomjs and libprotobuf

---
+++ phantomjs/bld.bat
@@ -0,0 +1,4 @@
+copy bin\phantomjs.exe %LIBRARY_BIN%\
+copy LICENSE.BSD %LIBRARY_BIN%\phantomjs_license
+
+rem vim:set ts=8 sw=4 sts=4 tw=78 et:
---
+++ phantomjs/build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mkdir -p $PREFIX/bin
+cp -r bin/phantomjs $PREFIX/bin/
+cp LICENSE.BSD $PREFIX/bin/phantomjs_license
