ea00b694abdbad3560da2b7cc719a84f8527d7b1
Greg Brener <gbrener@continuum.io>
2016-09-20 16:42:25 -0500

    Add cyrus-sasl as separate package. Make various improvements based on suggestions from others. Remove comments from conda-skeleton.

--- sasl/build.sh
+++ sasl/build.sh
@@ -1,18 +1,4 @@
 #!/bin/bash
 
-curl -O ftp://ftp.cyrusimap.org/cyrus-sasl/cyrus-sasl-2.1.26.tar.gz
-tar -xvzf cyrus-sasl-2.1.26.tar.gz
-pushd cyrus-sasl-2.1.26
-./configure --prefix=$PREFIX --with-des=no
-make
-make install
-popd
-
 $PYTHON setup.py build_ext --include-dirs=${PREFIX}/include --library-dirs=${PREFIX}/lib
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
-
-# Add more build steps here, if they are necessary.
-
-# See
-# http://docs.continuum.io/conda/build.html
-# for a list of environment variables that are set during the build process.
