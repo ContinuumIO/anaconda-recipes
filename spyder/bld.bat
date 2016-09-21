cab091ed2886ea838d0013ba96d16ae3f40566ce
Ilan Schnell <ilanschnell@gmail.com>
2016-09-21 16:41:36 -0500

    minor fixes and updates

--- spyder/bld.bat
+++ spyder/bld.bat
@@ -6,8 +6,8 @@ IF NOT EXIST (%MENU_DIR%) mkdir %MENU_DIR%
 
 copy %SRC_DIR%\img_src\spyder.ico %MENU_DIR%\
 if errorlevel 1 exit 1
-copy %SRC_DIR%\img_src\spyder_light.ico %MENU_DIR%\
-if errorlevel 1 exit 1
+REM copy %SRC_DIR%\img_src\spyder_light.ico %MENU_DIR%\
+REM if errorlevel 1 exit 1
 copy %RECIPE_DIR%\menu-windows.json %MENU_DIR%\spyder_shortcut.json
 if errorlevel 1 exit 1
 
