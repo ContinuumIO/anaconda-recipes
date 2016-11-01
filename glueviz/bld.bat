acc6146b03c438dc1e8628181aa215dd86430d4d
Ilan Schnell <ilanschnell@gmail.com>
2016-11-01 14:32:00 -0500

    add constantly and incremental recipes, update glueviz and Twisted

--- glueviz/bld.bat
+++ glueviz/bld.bat
@@ -1,6 +1,9 @@
 %PYTHON% setup.py install
 if errorlevel 1 exit 1
 
+del %SCRIPTS%\easy_install*
+
 if %PY3K%==1 (
     rd /s /q %SP_DIR%\__pycache__
+    rd /s /q %SP_DIR%\PyQt5
 )
---
+++ twisted/run_test.py
@@ -0,0 +1,4 @@
+import twisted
+
+print('twisted.__version__: %s' % twisted.__version__)
+assert twisted.__version__ == '16.5.0'
