86aa35ee65134137b0b60d23e64520fd49c693da
Ilan Schnell <ilanschnell@gmail.com>
2016-11-09 17:58:04 -0600

    add conda-verify recipe, update several others

--- constructor/run_test.py
+++ constructor/run_test.py
@@ -1,7 +1,7 @@
 import constructor
 
 print('constructor version:', constructor.__version__)
-assert constructor.__version__ == '1.4.2'
+assert constructor.__version__ == '1.5.0'
 
 import constructor.tests
 constructor.tests.main()
