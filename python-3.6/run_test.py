a1176760a9bf12d537dc2ca896e5214cf831a85e
Ilan Schnell <ilanschnell@gmail.com>
2017-01-27 14:55:56 -0600

    add isort and subprocess32 recipes, update line_profiler and a few tests

--- python-3.6/run_test.py
+++ python-3.6/run_test.py
@@ -132,4 +132,4 @@ if not (armv6l or armv7l or ppc64le):
 
 print('OPENSSL_VERSION:', ssl.OPENSSL_VERSION)
 if sys.platform != 'win32':
-    assert '1.0.2j' in ssl.OPENSSL_VERSION
+    assert '1.0.2k' in ssl.OPENSSL_VERSION
