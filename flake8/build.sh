c2a257b74629aac1a62a3817490ac8e582acf01c
Ilan Schnell <ilanschnell@gmail.com>
2017-02-15 22:05:39 -0600

    update anaconda-client, distributed, flake8, ipython, jupyter_core, mccabe, nbconvert, passlib, path.py, pillow, setuptools_scm and statsmodels recipes

--- flake8/build.sh
+++ flake8/build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-$PYTHON setup.py install
+$PYTHON setup.py egg_info
+$PYTHON setup.py install --old-and-unmanageable
--- nbconvert/build.sh
+++ nbconvert/build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+rm -rf nbconvert/preprocessors/tests/files/
+
 $PYTHON setup.py install --old-and-unmanageable
