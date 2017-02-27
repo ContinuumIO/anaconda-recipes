b66b70f76da7ffa2655fe5eace31883dffeac870
Ilan Schnell <ilanschnell@gmail.com>
2017-02-27 12:02:43 -0600

    update conda and IPython

--- conda-4.3/cio_test.py
+++ conda-4.3/cio_test.py
@@ -3,15 +3,18 @@ import os
 
 level = int(os.getenv('CIO_TEST') or 0)
 
+BREMEN = 'bremen'
+
 base_urls = []
 if level <= 3:
     if level >= 1:
-        base_urls.extend(['http://bremen/pkgs/pro',
-                          'http://bremen/pkgs/free'])
+        base_urls.extend(['http://%s/pkgs/pro' % BREMEN,
+                          'http://%s/pkgs/free' % BREMEN])
     if level >= 2:
-        base_urls.insert(0, 'http://bremen/test-pkgs')
+        base_urls.insert(0, 'http://%s/test-pkgs' % BREMEN)
     if level == 3:
-        base_urls.insert(0, 'http://bremen/test-pkgs-%s' % os.getenv('USER'))
+        base_urls.insert(0, 'http://%s/test-pkgs-%s' %
+                         (BREMEN, os.getenv('USER')))
 
 if level == 4:
     base_urls = ['https://repo.continuum.io/pkgs/free',
--- conda-4.3/run_test.py
+++ conda-4.3/run_test.py
@@ -2,6 +2,6 @@ import conda
 import cio_test
 
 print('conda.__version__: %s' % conda.__version__)
-assert conda.__version__ == '4.3.13'
+assert conda.__version__.startswith('4.3.13')
 
 from conda.fetch import handle_proxy_407
