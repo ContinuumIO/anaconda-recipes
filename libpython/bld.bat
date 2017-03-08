be2a4d3c1f3ce978f145aa573465d77da8419953
Ilan Schnell <ilanschnell@gmail.com>
2017-03-08 11:07:05 -0600

    update Mercurial recipe, fixes to libpython

--- libpython/bld.bat
+++ libpython/bld.bat
@@ -1,12 +1,12 @@
 :: Create an empty library for msvcrt??? since CygwinCCompiler seems to
 :: think that linking to that is a good idea (it is not).
-if "%CONDA_PY%" == "35" (
-  ar cru %PREFIX%\libs\libmsvcr140.dll.a
+if "%CONDA_PY%" == "27" (
+  ar cru %PREFIX%\libs\libmsvcr90.dll.a
 ) else (
   if "%CONDA_PY%" == "34" (
     ar cru %PREFIX%\libs\libmsvcr120.dll.a
   ) else (
-    ar cru %PREFIX%\libs\libmsvcr90.dll.a
+    ar cru %PREFIX%\libs\libmsvcr140.dll.a
   )
 )
 gendef.exe %PREFIX%\python%CONDA_PY%.dll - > python%CONDA_PY%.def
