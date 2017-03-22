35a9640ea7b1755cc0a73cb54d2eed1e0d3f0c32
Ilan Schnell <ilanschnell@gmail.com>
2017-03-22 17:41:26 -0500

    update conda, dask, distributed and lz4

--- conda-4.3/run_test.py
+++ conda-4.3/run_test.py
@@ -1,6 +1,6 @@
 import conda
 
 print('conda.__version__: %s' % conda.__version__)
-assert conda.__version__.startswith('4.3.14')
+assert conda.__version__.startswith('4.3.15')
 
 from conda.fetch import handle_proxy_407
--- lz4/run_test.py
+++ lz4/run_test.py
@@ -2,6 +2,6 @@ import lz4
 
 data = 100 * b"Hello World!"
 
-compressed_data = lz4.dumps(data)
+compressed_data = lz4.block.compress(data)
 print(len(data), len(compressed_data))
-assert data == lz4.loads(compressed_data)
+assert data == lz4.block.decompress(compressed_data)
