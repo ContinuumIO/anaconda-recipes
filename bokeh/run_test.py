129531b8b7a9a25608074a89ce8bb44cbdb67e17
Ilan Schnell <ilanschnell@gmail.com>
2017-04-05 23:00:28 -0500

    update bokeh, botocore, jupyter_client, notebook, Pillow, sortedcollections and wrapt recipes

--- bokeh/run_test.py
+++ bokeh/run_test.py
@@ -1,8 +1,4 @@
-import sys
 import bokeh
 
-#if sys.platform.startswith('linux'):
-#    bokeh.test()
-
 print('bokeh.__version__: %s' % bokeh.__version__)
-assert bokeh.__version__ == '0.12.4'
+assert bokeh.__version__ == '0.12.5'
