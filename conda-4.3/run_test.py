61163f75cd4ceb1f7c9e2422a21af8121bb98168
Ilan Schnell <ilanschnell@gmail.com>
2017-05-09 18:20:43 -0500

    update botocore, conda, conda-build and pyodbc recipes

--- conda-4.3/run_test.py
+++ conda-4.3/run_test.py
@@ -1,6 +1,6 @@
 import conda
 
 print('conda.__version__: %s' % conda.__version__)
-assert conda.__version__ == '4.3.17'
+assert conda.__version__ == '4.3.18'
 
 from conda.fetch import handle_proxy_407
