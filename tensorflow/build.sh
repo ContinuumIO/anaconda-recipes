b300d6be63cc705e92125dabb77fb6a57df04c45
Ilan Schnell <ilanschnell@gmail.com>
2017-05-23 09:56:32 -0500

    update tensorflow recipe

--- tensorflow/build.sh
+++ tensorflow/build.sh
@@ -2,20 +2,28 @@
 
 set -ex
 
+# Compile tensorflow from source
 export PYTHON_BIN_PATH=${PYTHON}
-# Disable Google cloud compute
+export PYTHON_LIB_PATH=${SP_DIR}
+export CC_OPT_FLAGS="-march=nocona"
+# disable jemmloc (needs MADV_HUGEPAGE macro which is not in glib <= 2.12)
+export TF_NEED_JEMALLOC=0
 export TF_NEED_GCP=0
-# Disable gpu for now
+export TF_NEED_HDFS=0
+export TF_ENABLE_XLA=0
+export TF_NEED_OPENCL=0
 export TF_NEED_CUDA=0
 ./configure
 
 export CFLAGS="-I${PREFIX}/include -L${PREFIX}/lib $CFLAGS"
 export CXXFLAGS="-I${PREFIX}/include -L${PREFIX}/lib $CXXFLAGS"
 
-bazel build -c opt //tensorflow/tools/pip_package:build_pip_package
+# build using bazel
+bazel build --config=opt //tensorflow/tools/pip_package:build_pip_package
 
-mkdir _python_build
-cd _python_build
-ln -s ../bazel-bin/tensorflow/tools/pip_package/build_pip_package.runfiles/org_tensorflow/* .
-ln -s ../tensorflow/tools/pip_package/* .
-python setup.py install --single-version-externally-managed --record=record.txt
+# build a whl file
+mkdir $SRC_DIR/tensorflow_pkg
+bazel-bin/tensorflow/tools/pip_package/build_pip_package $SRC_DIR/tensorflow_pkg
+
+# install using pip from the whl file
+pip install --no-deps $SRC_DIR/tensorflow_pkg/*.whl
