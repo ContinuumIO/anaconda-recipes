66c1c1f69108fd60ca0348aaf4889534d91e7048
Ilan Schnell <ilanschnell@gmail.com>
2017-05-25 14:41:56 -0500

    update conda recipe, and fix/update about section of many recipes

--- conda-4.3/run_test.py
+++ conda-4.3/run_test.py
@@ -1,6 +1,6 @@
 import conda
 
 print('conda.__version__: %s' % conda.__version__)
-assert conda.__version__ == '4.3.20'
+assert conda.__version__ == '4.3.21'
 
 from conda.fetch import handle_proxy_407
